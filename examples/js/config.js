79f27fc12481a7299061aaa589abb00f230fad93
wen-ddd <22589695@qq.com>
2021-01-12 18:34:54 +0800

    first commit

---
+++ examples/js/config.js
@@ -0,0 +1,7 @@
+var URL_CONFIG = {
+
+	BING_MAP_KEY: 'AjZQFTIC1djVRAMMYgiekFSPjfqT_8qCjMrOhKQMEzOsUd-_zjIPEJbcm2vO2Xec',
+	// SCENE_CBD : 'http://www.supermapol.com/realspace/services/3D-CBDCache20200416/rest/realspace' ,// CBD场景
+	SCENE_CBD: 'http://localhost:8090/iserver/services/3D-qilin/rest/realspace' // CBD场景
+
+};
